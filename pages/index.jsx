86f5a9ef617cbb67420ffba5b0bd86ec0e2987db
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2021-10-30 15:28:17 +0200

    dynamic  imports

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,8 +2,16 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
 import Link from 'next/link'
+/**
+ * Next.js supports ES2020 dynamic import() for JavaScript. 
+ * With it you can import JavaScript modules dynamically and work with them. They also work with SSR.
+ * You can think of dynamic imports as another way to split your code into manageable chunks.
+ */
+import dynamic from 'next/dynamic'
 import Navigation from '../src/components/Navigation'
 import styles from '../src/styles.module.css'
+
+const DynamicComponent = dynamic(() => import('../src/components/Hello'))
 
 // Global CSS cannot be imported from files other than your Custom <App>.
 // import '../src/styles.css'
@@ -18,6 +26,7 @@ const Page = ({ content }) => (
       }}
     >
       <h1 sx={{ fontSize: 8, my: 0 }}>{content.title}</h1>
+      <DynamicComponent />
     </div>
   </div>
 )
@@ -36,4 +45,15 @@ export async function getStaticProps() {
 /**
  * Each page is associated with a route based on its file name.
  * For example, If you create pages/about.js that exports a React component will be accessible at /about.
+ */
+
+/**
+ * REAL-LIFE SCENARIOS
+Here are some typical components that would be great for dynamic imports:
+
+- UI components such as modals, drawers, and toggles that don’t display content until an interaction occurs
+- Search components that rely on some form of text input or button click
+- “Load More” components that render additional content on user interaction
+- Module queries from an external API
+- Components that are slow loading and could benefit from a loading component while the import resolves
  */
